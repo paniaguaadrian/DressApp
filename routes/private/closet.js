9ea2c3358a198bf2b4afbe30f12ffe3436e51c5f
Eric Blair <erbmart7@gmail.com>
2020-10-30 18:11:37 +0100

    populate working

--- routes/private/closet.js
+++ routes/private/closet.js
@@ -8,13 +8,30 @@ const withAuth = require("../../middleware/auth");
 
 router.get('/', withAuth, async (req,res,next) =>{
   if (req.userID) {
-
+    try {
+  
       const userUpdated = await User.findById(req.userID)
-
+      .populate('items')
+      .exec()
       res.locals.currentUserInfo = userUpdated;
+      console.log( res.locals.currentUserInfo)
+      // let itemQuery = {items: res.locals.currentUserInfo.items}
+      
 
-    const allItems = await Item.find()
-    res.render('private/closet/index.hbs', {allItems})
+      // const itemDocs = await User.find(itemQuery)
+      // .populate('items')
+      // .exec()
+
+    res.render('private/closet/index.hbs', {userUpdated})
+    } catch (error) {
+        next(error);
+        return;
+    }
+  } else {
+    res.redirect('/')
+  // en caso contrario (si no hay token) redirigimos a la home
+  // otra opción es definir la respuesta con status 401, y renderizamos nuestra vista 'home' con un errorMessage ('Unauthorized: No token provided')
+
   }
 })
 
@@ -22,11 +39,13 @@ router.get('/add-item', withAuth, async (req,res,next) =>{
     res.render('private/closet/create.hbs')
 })
 
-router.post('/add-item', topCloud.single("photo"), async (req,res,next) =>{
+router.post('/add-item', withAuth, topCloud.single("photo"), async (req,res,next) =>{
     const {name, description, type, brand, price} = req.body
     const image = req.file.url
     try {
+      console.log(req.userID)
       const newItem = await Item.create({name, description, image, type, brand, price})
+      await User.findByIdAndUpdate(req.userID, {items: newItem})
       res.redirect('/mycloset/add-item')
     } catch (error) {
       console.log(error);
