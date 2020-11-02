b991948c56bbfcba8c7929df815b26eaa4815dbd
Adrián Paniagua <paniaguasanchezadrian@gmail.com>
2020-11-02 11:04:14 +0100

    Profile is working at 70%

--- routes/private/profile.js
+++ routes/private/profile.js
@@ -5,27 +5,29 @@ const topCloud = require("../../config/cloudinary");
 const withAuth = require("../../middleware/auth");
 
 router.get("/", withAuth, async (req, res, next) => {
-  res.render("private/profile");
+  const user = await User.findById(req.userID);
+  res.render("private/profile", { user });
 });
 
 router.get("/edit-profile", withAuth, async (req, res, next) => {
-  let user = await User.findById(req.userID);
+  const user = await User.findById(req.userID);
   res.render("private/edit-profile.hbs", { user });
-  // console.log(`user: ${user}`);
 });
 
-router.post("/edit-profile", async (req, res, next) => {
-  const updatedUser = {
-    description: req.body.description,
-  };
-  let { description } = req.body;
-  console.log(`req.body.description: ${req.body.description}`);
-  User.update({ _id: req.userID }, updatedUser);
-  console.log(`updatedUser: ${updatedUser}`);
-  console.log(`req.userID: ${req.userID}`);
-
-  // await User.findByIdAndUpdate({ $set: { description } });
-  res.redirect("/myprofile");
-});
+router.post(
+  "/edit-profile",
+  withAuth,
+  topCloud.single("photo"),
+  async (req, res, next) => {
+    const { description } = req.body;
+    console.log(req.body);
+    // const image = req.file.url;
+    // console.log(`photo: ${image}`);
+    await User.findByIdAndUpdate(req.userID, {
+      description: description,
+    });
+    res.redirect("/myprofile");
+  }
+);
 
 module.exports = router;
