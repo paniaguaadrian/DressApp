6414c0a1210e713d9284e7b426318cd49bf0d047
Adrián Paniagua <paniaguasanchezadrian@gmail.com>
2020-11-04 01:34:10 +0100

    Styled profile, editProfile, community, myCloset, Clothes, outfits, collections

--- routes/private/closet.js
+++ routes/private/closet.js
@@ -52,7 +52,9 @@ router.post(
         price,
       });
       await User.findByIdAndUpdate(req.userID, { $push: { items: newItem } });
-      res.render("private/closet/item/create.hbs", {successMessage: `${newItem.name} has been added successfully`});
+      res.render("private/closet/item/create.hbs", {
+        successMessage: `${newItem.name} has been added successfully`,
+      });
     } catch (error) {
       console.log(error);
     }
@@ -84,11 +86,24 @@ router.get("/add-outfit", withAuth, async (req, res, next) => {
 router.post("/add-outfit", withAuth, async (req, res, next) => {
   const { name, description, imageTop, imageBottom, imageFeet } = req.body;
   try {
-    console.log('These are the outfits ' + imageTop + ' and ' + imageBottom + ' and ' + imageFeet)
-    const newOutfit= await Outfit.create({name, description, imageTop, imageBottom, imageFeet})
-    await User.findByIdAndUpdate(req.userID, {$push:{outfits: newOutfit}})
+    console.log(
+      "These are the outfits " +
+        imageTop +
+        " and " +
+        imageBottom +
+        " and " +
+        imageFeet
+    );
+    const newOutfit = await Outfit.create({
+      name,
+      description,
+      imageTop,
+      imageBottom,
+      imageFeet,
+    });
+    await User.findByIdAndUpdate(req.userID, { $push: { outfits: newOutfit } });
 
-    res.redirect('/mycloset/add-outfit')
+    res.redirect("/mycloset/add-outfit");
   } catch (error) {
     console.log(error);
   }
@@ -102,31 +117,36 @@ router.get("/add-collection", withAuth, async (req, res, next) => {
 router.post("/add-collection", withAuth, async (req, res, next) => {
   const { name, description } = req.body;
   try {
-    const newCollection= await Collection.create({name, description})
-    await User.findByIdAndUpdate(req.userID, {$push:{collections: newCollection}})
-    res.redirect('/mycloset/add-collection')
+    const newCollection = await Collection.create({ name, description });
+    await User.findByIdAndUpdate(req.userID, {
+      $push: { collections: newCollection },
+    });
+    res.redirect("/mycloset/add-collection");
   } catch (error) {
     console.log(error);
   }
 });
 
-router.get('/:id/edit-item', withAuth, async (req, res, next) => {
-    try{
-       let item = await Item.findById(req.params.id)
-        console.log('Retrieved item with id:', item);
-        res.render('private/closet/item/edit.hbs', {item});
-    }catch(err){
-        console.log('Error while editing the item: ', err);
-    }
-  });
-  
-  router.post('/:id/edit-item', topCloud.single("photo"), async (req, res, next) => {
-    let {name, description, imageBefore, type, brand, price} = req.body;
+router.get("/:id/edit-item", withAuth, async (req, res, next) => {
+  try {
+    let item = await Item.findById(req.params.id);
+    console.log("Retrieved item with id:", item);
+    res.render("private/closet/item/edit.hbs", { item });
+  } catch (err) {
+    console.log("Error while editing the item: ", err);
+  }
+});
 
-    if(req.file){
-      image = req.file.url
-    } else if(!req.file || req.file === ''){
-      image = imageBefore
+router.post(
+  "/:id/edit-item",
+  topCloud.single("photo"),
+  async (req, res, next) => {
+    let { name, description, imageBefore, type, brand, price } = req.body;
+
+    if (req.file) {
+      image = req.file.url;
+    } else if (!req.file || req.file === "") {
+      image = imageBefore;
     }
 
     await Item.findByIdAndUpdate(
@@ -137,39 +157,43 @@ router.get('/:id/edit-item', withAuth, async (req, res, next) => {
   }
 );
 
-  router.get('/:id/edit-outfit', withAuth, async (req, res, next) => {
-    const thisUser = await User.findById(req.userID)
-    .populate('items')
-    .exec()
-    const outfit = req.params.id
-    let topItems = []
-    let bottomItems = []
-    let feetItems = []
-    const userItems = thisUser.items
-    userItems.forEach(function(item) {
-      if(item.type === 'top'){
-        topItems.push(item)
-      }else if (item.type === 'bottom'){
-        bottomItems.push(item)
-      }else if (item.type === 'feet'){
-        feetItems.push(item)
-      }
-    })
-    try{
-        res.render('private/closet/outfit/edit.hbs', {outfit, topItems, bottomItems,feetItems});
-    }catch(err){
-        console.log('Error while editing the item: ', err);
+router.get("/:id/edit-outfit", withAuth, async (req, res, next) => {
+  const thisUser = await User.findById(req.userID).populate("items").exec();
+  const outfit = req.params.id;
+  let topItems = [];
+  let bottomItems = [];
+  let feetItems = [];
+  const userItems = thisUser.items;
+  userItems.forEach(function (item) {
+    if (item.type === "top") {
+      topItems.push(item);
+    } else if (item.type === "bottom") {
+      bottomItems.push(item);
+    } else if (item.type === "feet") {
+      feetItems.push(item);
     }
   });
-  
-  router.post('/:id/edit-outfit', async (req, res, next) => {
-    console.log(req.body)
-    let {name, description, imageTop, imageBottom, imageFeet} = req.body;
-    await Outfit.findByIdAndUpdate(
-        { _id: req.params.id },
-        { $set: { name, description, imageTop, imageBottom, imageFeet} })
-          res.redirect("/mycloset");  
-  });
+  try {
+    res.render("private/closet/outfit/edit.hbs", {
+      outfit,
+      topItems,
+      bottomItems,
+      feetItems,
+    });
+  } catch (err) {
+    console.log("Error while editing the item: ", err);
+  }
+});
+
+router.post("/:id/edit-outfit", async (req, res, next) => {
+  console.log(req.body);
+  let { name, description, imageTop, imageBottom, imageFeet } = req.body;
+  await Outfit.findByIdAndUpdate(
+    { _id: req.params.id },
+    { $set: { name, description, imageTop, imageBottom, imageFeet } }
+  );
+  res.redirect("/mycloset");
+});
 
 router.post("/:id/edit-outfit", async (req, res, next) => {
   console.log(req.body);
@@ -201,18 +225,21 @@ router.post("/:id/edit-collection", async (req, res, next) => {
 router.get("/:id/add-collection", withAuth, async (req, res, next) => {
   const thisUser = await User.findById(req.userID).populate("outfits").exec();
   try {
-    const collection = await Collection.findById(req.params.id)
-    res.render(`private/closet/collection/add.hbs`, {thisUser, collection})
+    const collection = await Collection.findById(req.params.id);
+    res.render(`private/closet/collection/add.hbs`, { thisUser, collection });
   } catch (error) {
     console.log(error);
   }
 });
 
 router.post("/:id/add-collection", withAuth, async (req, res, next) => {
-  const {outfit} = req.body
+  const { outfit } = req.body;
   try {
-    await Collection.findByIdAndUpdate({ _id: req.params.id }, { $push: { outfits: outfit } })
-    res.redirect(`/mycloset/${req.params.id}/add-collection`)
+    await Collection.findByIdAndUpdate(
+      { _id: req.params.id },
+      { $push: { outfits: outfit } }
+    );
+    res.redirect(`/mycloset/${req.params.id}/add-collection`);
   } catch (error) {
     console.log(error);
   }
@@ -254,11 +281,16 @@ router.post("/:id/delete-collection", withAuth, async (req, res, next) => {
   }
 });
 
-router.post("/:id/delete-outfit-collection", withAuth, async (req, res, next) => {
+router.post(
+  "/:id/delete-outfit-collection",
+  withAuth,
+  async (req, res, next) => {
     const _id = req.body.outfitID;
     try {
       const collectionID = req.params.id;
-      await Collection.findByIdAndUpdate(collectionID, {$pull: { outfits: _id }});
+      await Collection.findByIdAndUpdate(collectionID, {
+        $pull: { outfits: _id },
+      });
       res.redirect(`/mycloset/${req.params.id}/edit-collection`);
     } catch (err) {
       console.log("Error while deleting the item: ", err);
